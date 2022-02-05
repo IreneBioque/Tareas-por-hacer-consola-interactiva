af04f5ab134cc9374da13b462f71bb9d8921dd5b
IreneBioque <irenebioque@gmail.com>
2022-02-05 11:32:41 +0100

    add inquirerMenu and Tasks

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('colors');
 
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+// const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const Task = require('./models/task');
+const Tasks = require('./models/tasks');
 
 const main = async () => {
   console.log('Hola mundo');
@@ -8,8 +11,11 @@ const main = async () => {
   let opt = '';
 
   do {
-    opt = await mostrarMenu();
-    console.log({ opt });
+    // opt = await inquirerMenu();
+    // console.log({ opt });
+    const tarea = new Task('Comer')
+    const tareas = new Tasks;
+    console.log(tarea)
     if (opt !== '0') await pausa();
   } while (opt !== '0');
 
