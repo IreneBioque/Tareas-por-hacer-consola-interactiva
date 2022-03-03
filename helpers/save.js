270506463a566f6292d5b19c26ebe7ecd5d90384
IreneBioque <irenebioque@gmail.com>
2022-03-03 19:00:59 +0100

    add save data base

---
+++ helpers/save.js
@@ -0,0 +1,26 @@
+//filesystem
+const fs = require("fs");
+
+const registry = "./db/data.json";
+
+const saveDB = (data) => {
+  fs.writeFileSync(registry, JSON.stringify(data));
+};
+
+const readDB = () => {
+
+    if (!fs.existsSync(registry)) {
+        return null;
+    }
+
+    const info = fs.readFileSync(registry, { encoding: 'utf-8' })
+    const data = JSON.parse(info);
+
+    return data;
+
+}
+
+module.exports = {
+  saveDB,
+  readDB,
+};
