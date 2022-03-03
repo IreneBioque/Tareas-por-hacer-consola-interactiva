5089bab1b90f38a46758f354d14d1972607fd689
IreneBioque <irenebioque@gmail.com>
2022-03-03 19:01:53 +0100

    add completed and pending list

--- models/tasks.js
+++ models/tasks.js
@@ -9,7 +9,6 @@ class Tasks {
   get listArr() {
     const listArray = [];
     Object.keys(this.list).forEach((key) => {
-      console.log(key);
       const tarea = this.list[key];
       listArray.push(tarea);
     });
@@ -21,11 +20,47 @@ class Tasks {
     this.list = {};
   }
 
+  upTask(tasks = []) {
+    tasks.forEach((task) => {
+      this.list[task.id] = task;
+    });
+  }
+
   doTask(desc = "") {
     const task = new Task(desc);
 
     this.list[task.id] = task;
   }
+
+  completedList() {
+    console.log();
+    this.listArr.forEach((task, i) => {
+      const idx = `${i + 1}`.green;
+      const { desc, completed } = task;
+      const state = completed ? "Completada".green : "Pendiente".red;
+      console.log(`${idx} ${desc} :: ${state}`);
+    });
+  }
+  pendingCompletedList(completeds = true) {
+    console.log();
+    let count = 0;
+    this.listArr.forEach((task, i) => {
+      const idx = `${i + 1}`.green;
+      const { desc, completed } = task;
+      const state = completed ? "Completada".green : "Pendiente".red;
+      if (completeds) {
+        if (completed) {
+          count += 1;
+          console.log(`${idx} ${desc} :: ${completed}`);
+        }
+      } else {
+        if (!completed) {
+          count += 1;
+          console.log(`${idx} ${desc} :: ${state}`);
+        }
+      }
+    });
+  }
 }
 
-module.exports = Tasks;
+module.exports = { Tasks };
